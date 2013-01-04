2259fd04fbd8be5c25b3722bf9a008fd1dd08681
euphoric.sg <euphoric.sg@gmail.com>
2013-01-04 17:58:20 +0000

    Debugged for malloc error

--- p1_miniprj/export.c
+++ p1_miniprj/export.c
@@ -1,14 +1,11 @@
 /* EXPORTING SEMANTICS */
 
-/* EXECUTING:
-	gcc export.c dbfunc.c -o export -l sqlite3
-	./export Tag_Name Path
-   EXAMPLE:
-	./export Folder1 /home/admin/Folder2
-*/
+/* gcc export.c dbfunc.c extract_audio_taglib.c -o export -lsqlite3 -ltag_c
+ * ./export tag_name absolute_path
+ */
 
 /* LICENSE
-   Copyright [2012] [harshvardhan pandit]
+   Copyright [2013] [sahil gupta]
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
@@ -20,142 +17,124 @@
    limitations under the License.
  */
 
-#include<stdio.h>
-#include<string.h>
-#include<dirent.h>
-#include<sys/stat.h>
-#include<errno.h>
-#include<fcntl.h>
-#include<sys/sendfile.h>
+#include "export.h"
 
-#include "dbfunc.h"	/* Database Functions */
+/* send_file_fun : copies file from source to destination
+ * param: char *source - source of file
+ * param: char *destination - path where file is to be copied
+ * return: 1 on SUCCESS
+ * author: @SG
+ */
+static int send_file(const char *source,const char *destination)
+{
+	int read_src;
+	int write_dst;
+	struct stat stat_buf;
+	off_t offset = 0;
+	char dst[_POSIX_PATH_MAX];
+	 
+	/* Open the input file */
+	read_src=open(source, O_RDONLY);
+	if(read_src == -1) {
+		printf("Cannot open source file : %s\n", source);
+		return EXPORT_COPY_FAIL;
+	}
+	 
+	/* Call Stat on input file to obtain its size */
+	if(fstat(read_src, &stat_buf) == -1) {
+		printf("Error accessing source file : %s\n", source);
+		return EXPORT_COPY_FAIL;
+	}
+	
+	/* Open the dstput file for writing, 
+	 * with the same permissions as the source file 
+	 */
+	strcpy(dst,destination);
+	strcat(dst,strrchr(source,'/'));
+	write_dst = open(dst , O_WRONLY | O_CREAT, stat_buf.st_mode);
+	if( write_dst == -1 ) {
+		printf("Cannot open destination file : %s\n", dst);
+		return EXPORT_COPY_FAIL;
+	}
 
-/* Database Functions Required:
- * node *getassociation(char *tag);
- * node *getfile(char *tag);
- * */
+	/* Blast the bytes from one file to the other */
+	if(sendfile(write_dst, read_src, &offset, stat_buf.st_size) == -1){
+		return EXPORT_COPY_FAIL; 
+	}
+	
+	/* Close Files */
+	close (read_src);
+	close (write_dst);
+	 
+	return EXPORT_COPY_SUCCESS;
+}
 
-int send_file_fun(char *source, char *destination);
+/* export: Exports a tag in kwest as Directory-file structure on FS
+ * param: char *tag - Tag name in Kwest
+ * param: char *path - Absolute Path of Location where tag is to be exported
+ * return: EXPORT_SUCCESS on SUCCESS
+ * author: @SG
+ */
+int export(const char *tag,const char *path)
+{
+	DIR *directory;
+	const char *filename,*tagname,*filepath;
+	char tagpath[_POSIX_PATH_MAX];
+	void *ptr;
+
+	/* Check if path is valid */
+	if((directory = opendir(path)) == NULL) {
+		printf("Error opening path\n");
+		return EXPORT_FAIL;
+	}
+	if(closedir(directory) != 0){
+		printf("Error closing path\n");
+	}
+
+	/* Form path for creating Directory */
+	strcpy(tagpath,path);
+	strcat(strcat(tagpath,"/"),tag);
+
+	/* Create Directory for tag */ 
+	printf("Creating Dir : %s\n",tag);
+	if(mkdir(tagpath,0777) == -1 && errno != EEXIST) {
+		printf("Error Creating Directory\n");
+		return EXPORT_FAIL;
+	} 
+
+	/* Copy Files in Directory */
+	ptr=get_file_abspath(tag); /* Get Path */
+	while((filepath=get_data(ptr))!=NULL) {
+		filename=strrchr(filepath,'/')+1; /* Filename */
+		if(send_file(filepath,tagpath)==EXPORT_COPY_FAIL){ 
+			printf("Error copying file %s\n",filename);
+		} else {
+			printf("Copy File : %s\n",filename);
+		}
+	}
+
+	/* Copy Sub-Directories */
+	ptr=get_association(tag,RELATION_SUBGROUP); /* Get Tag */
+	while((tagname=get_data(ptr))!=NULL) {
+		/* Copy Sub-Directory contents */
+		export(tagname,tagpath); 
+	}
+
+	return EXPORT_SUCCESS;
+} 
 
 int main(int argc,const char *argv[])
 {
-	 if(argc<3) return -1;
+	if(argc<3) return -1;
+	init_db();
 
-	 initdb();
-
-	 if(export_semantics(argv[1],argv[2]))
+	if(export(argv[1],argv[2]) == EXPORT_SUCCESS){
 		printf("Tag Exported\n");
-	 else
-		printf("Operation Failed\n");
+		close_db();
+		return EXPORT_SUCCESS;
+	}
 
-	 /* (void)closedb(); */
-	 return 0;
-}
-
-/* export_semantics:
- * param:	char *tag - Tag name in Kwest
- *			char *path - Absolute Path of Location where tag is to be exported
- * return:	1 on SUCCESS
- * author:  @SG
- * */
-int export_semantics(char *tag, char *path)
-{
-	 char *tagpath;
-	 char temppath[_POSIX_PATH_MAX];
-	 DIR *directory;
-	 node *filenode,*tagnode,*temp;
-	 
-	 /* Check if path is valid */
-	 if((directory = opendir(path)) == NULL) {
-		printf("Error opening path\n");
-		return 0;
-	 }
-	 if(closedir(directory) != 0)
-		printf("Error closing path\n");
-	 
-	 /* Create Directory for tag */ 
-	 printf("Creating Dir : %s\n",tag);
-	 tagpath = strcat(strcat(strdup(path),"/"),tag);
-	 if(mkdir(tagpath,0777) == -1 && errno != EEXIST) {
-		printf("Error Creating Directory\n");
-		return 0;
-	 } 
-
-	 /* Copy Files in Directory */
-	 if((filenode=getfile(tag))!=NULL) {
-		temp=filenode;
-		strcpy(temppath,tagpath);
-		strcat(temppath,"/");
-		while(filenode) {
-			printf("Copying File : %s\n",filenode->data);
-			if(!send_file_fun(filenode->data,temppath)) 
-				printf("There was an error copying file %s\n",filenode->data);
-			filenode=filenode->next;
-		}
-		while(temp) { /* Deallocate Memory */
-			filenode=temp->next;
-			free(temp);
-			temp=filenode;
-		} 
-	 }
-
-	 /* Create Sub-Directory */ 
-	 if((tagnode=getassociation(tag,"subgroup"))!=NULL) {
-		temp=tagnode;
-		while(tagnode) {	
-			export_semantics(tagnode->data,tagpath);
-			tagnode=tagnode->next;
-		}
-		while(temp)	{ /* Deallocate Memory */
-			tagnode=temp->next;
-			free(temp);
-			temp=tagnode;
-		} 
-	 }
-	 
-	 return 1;
-} 
-
-/* send_file_fun : copies file from source to destination
- * param:	char *source - source of file
- *	 		char *destination - path where file is to be copied
- * return: 	1 on SUCCESS
- * author: 	@SG
- * */
-int send_file_fun(char *source, char *destination)
-{
-	 int read_fd;
-	 int write_fd;
-	 struct stat stat_buf;
-	 off_t offset = 0;
-	 char out[_POSIX_PATH_MAX];
-
-	 /* Open the input file */
-	 if((read_fd=open(source, O_RDONLY)) == -1) {
-		printf("Cannot open %s\n", source);
-		return 0;
-	 }
-	 
-	 /* Stat the input file to obtain its size */
-	 if( fstat (read_fd, &stat_buf) == -1 ) {
-		printf("Error fstat open %s\n", source);
-		return 0;
-	 }
-
-	 /* Open the output file for writing, with the same permissions as the source file */
-	 strcpy(out,destination); 
-	 if( (write_fd = open(strcat(out,strrchr(source,'/')+1), O_WRONLY | O_CREAT, stat_buf.st_mode)) == -1 ) {
-		printf("Cannot open dest %s\n", destination);
-		return 0;
-	 }
-
-	 /* Blast the bytes from one file to the other */
-	 if(sendfile(write_fd, read_fd, &offset, stat_buf.st_size) == -1)
-		return 0; 
-
-	 /* Close Files */
-	 close (read_fd);
-	 close (write_fd);
-	 
-	 return 1;
+	printf("Operation Failed\n");
+	close_db(); 
+	return EXPORT_FAIL;
 }
