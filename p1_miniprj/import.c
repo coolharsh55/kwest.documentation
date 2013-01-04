f23f78e10ee908b9d51540448c365157149c186e
euphoric.sg <euphoric.sg@gmail.com>
2013-01-04 17:55:05 +0000

    integrated with kwest_main

--- p1_miniprj/import.c
+++ p1_miniprj/import.c
@@ -1,14 +1,8 @@
 /* IMPORTING SEMANTICS */
-
-/* EXECUTING:
-	gcc import.c dbfunc.c -o import -l sqlite3
-	./import directory_path
-   EXAMPLE: 
-	./import /home/admin/Folder1
-*/
+/* gcc -Wall -c import.c -o import.o -lsqlite3 */
 
 /* LICENSE
-   Copyright [2012] [harshvardhan pandit]
+   Copyright [2013] [sahil gupta]
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
@@ -20,103 +14,106 @@
    limitations under the License.
  */
 
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <dirent.h>
-
-#include "dbfunc.h"	/* Datadase Functions */
-
-/* Database Functions Required:
- * int initdb();
- * int closedb();
- * int addtag(char *tagname)
- * int addfile(char *abspath)
- * int tagfile(char *t,char *f)
- * int addassociation(char *t1,char *t2,char *relationtype)
- * */
-
-int main(int argc, const char *argv[])
-{
-	 if (argc < 2) return -1;
-
-	 initdb();
-
-	 /* Extract Directory name from path */
-	 char *dirname=strrchr(argv[1],'/');
-
-	 if(addtag(dirname+1)==1)
-		printf("Creating Tag : %s\n",dirname+1);
-
-	 if (import_semantics(argv[1],dirname+1))
-		printf("Tag Imported\n");
-	 else
-		printf("Operation Failed\n");
-
-	 closedb();
-	 return 0;
-}
+#include "import.h"
 
 /* import_semantics:
- * param:	char *path - Absolute path of Diectory to be imported
- *			char *dirname - Name of Directory 
- * return:	1 on SUCCESS
- * author: 	@SG
- * */
-int import_semantics(char *path, char *dirname)
+ * param: char *path - Absolute path of Diectory to be imported
+ * param: char *dirname - Name of Directory 
+ * return: 1 on SUCCESS
+ * author: @SG
+ */
+static int import_semantics(const char *path,const char *dirname)
 {
-	 DIR *directory;
-	 struct dirent *entry; 
-	 size_t path_len = strlen(path);
+	char full_name[_POSIX_PATH_MAX + 1];
+	DIR *directory;
+	struct dirent *entry; 
+	struct stat fstat;
+	size_t path_len;
+	size_t dir_len;
 	 
+	 path_len = strlen(path);
 	 directory = opendir(path);
-	 if (directory != NULL) {
-		while (entry = readdir (directory)) {
-			struct stat fstat;
-			char full_name[_POSIX_PATH_MAX + 1];
-			size_t dir_len = strlen(entry->d_name);
+	 if (directory == NULL) {
+		perror ("Couldn't open the directory");
+		return IMP_FAIL;
+	}
 
-			/* Calculate full name, check we are in file length limts */
-			if ((path_len + dir_len + 1) > _POSIX_PATH_MAX)
-				continue;
-
-			strcpy(full_name, path);
-			if (full_name[path_len - 1] != '/') 
-				strcat(full_name, "/");
-			strcat(full_name, entry->d_name);
-
-			/* Ignore files starting with . */
-			if((strchr(entry->d_name,'.')-entry->d_name)==0)
-				continue;
-
-			/* Ignore files ending with ~ */
-			if((strrchr(entry->d_name,'~')-entry->d_name)==dir_len-1)
-				continue;
+	while ((entry = readdir(directory))) {
+		dir_len = strlen(entry->d_name);
 		
-			/* Get File Infromation : Returns 0 if successful */
-			if (stat(full_name, &fstat) < 0)
-				continue;
-		
-			if (S_ISDIR(fstat.st_mode)) {			/* Directory */
-				if(addtag(entry->d_name)==1)
-					printf("Creating Tag : %s\n",entry->d_name);
-									
-				/* Access Sub-Directories */
-				(void) import_semantics(full_name,entry->d_name);
-				/* Tag-Tag Relation */
-				addassociation(entry->d_name,dirname,"subgroup");
+		/* Calculate full name, check we are in file length limts */
+		if ((path_len + dir_len + 1) > _POSIX_PATH_MAX){
+			continue;
+		}
+		strcpy(full_name, path);
+		if (full_name[path_len - 1] != '/'){
+			strcat(full_name, "/");
+		}
+		strcat(full_name, entry->d_name);
 
-			} else if(S_ISREG(fstat.st_mode)) {		/* Regular File */
-				if(addfile(full_name)==1)
-					printf("Adding File  : %s\n",entry->d_name);	 
+		/* Ignore files starting with . */
+		if((strchr(entry->d_name,'.')-entry->d_name)==0){
+			continue;
+		}
+
+		/* Ignore files ending with ~ */
+		if((strrchr(entry->d_name,'~')-entry->d_name)==dir_len-1){
+			continue;
+		}
+
+		/* Get File Infromation : Returns 0 if successful */
+		if (stat(full_name, &fstat) < 0){
+			continue;
+		}
+
+		if (S_ISDIR(fstat.st_mode)) { /* Directory */
+
+			if(add_tag(entry->d_name,USER_TAG)==KWEST_SUCCESS){
+				printf("Creating Tag : %s\n",entry->d_name);
+			}
+
+			/* Access Sub-Directories */
+			import_semantics(full_name,entry->d_name);
+
+			/* Tag-Tag Relation */
+			add_association(entry->d_name,dirname,
+			                RELATION_SUBGROUP);
+
+		} else if(S_ISREG(fstat.st_mode)) { /* Regular File */
+
+			if(add_file(full_name)==KWEST_SUCCESS){
+				printf("Adding File  : %s\n",entry->d_name);
+
 				/* Tag-File Relation */
-				tagfile(dirname,full_name);	
+				tag_file(dirname,full_name);
 			}
 		}
-		(void) closedir (directory);
-		return 1;
-	 } 
-	 else
-		perror ("Couldn't open the directory");     
-	 return 0;
+	}
+
+	(void) closedir (directory);
+	return IMP_SUCCESS;
+}
+
+/* import: Import Directory-File structure from File System to Kwest
+ * param: char *path - Absolute path of Diectory to be imported
+ * return: 1 on SUCCESS
+ * author: @SG
+ */
+int import(const char *path)
+{
+	/* Extract Directory name from path */
+	const char *dirname=strrchr(path,'/');
+
+	/* Create Tag for directory to be imported */
+	if(add_tag(dirname+1,USER_TAG)==KWEST_SUCCESS){
+		printf("Creating Tag : %s\n",dirname+1);
+	}
+
+	if (import_semantics(path,dirname+1)==IMP_SUCCESS) {
+		printf("Tag Imported\n");
+		return IMP_SUCCESS;
+	}
+
+	printf("Operation Failed\n");
+	return IMP_FAIL;
 }
